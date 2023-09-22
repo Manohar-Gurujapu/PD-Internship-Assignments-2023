02eea8102cdab51c41844faa65a0bd5ab6d113f6
Manohar-Gurujapu <gurujapu,manohar@prodevans.com>
2023-09-22 11:54:13 +0530

    Updated

--- script.js
+++ script.js
@@ -1,62 +1,104 @@
-const token = 'BQAJV_ZJ0XXTe-USb-TNyRlRsL1bO8f4XdEekbUu_usuVUKVcsjoHs8S4x56EsEQ39USXGMNZOw7P636mL617nohzDOFZd9k5hwFGVzyI7Hwee86JpT82nn8i-h5G_tQ7zqCsdGvN7yXeRRO0Tm8-Gs34seLuqlc8t0eYA7dMzyfLGRnRn5NXzCMU99N-hU2NCqKJXuSTS9yp_KNQW2tUGzXt1h1wabOUaDbClEdqZ-FkDkzS4arFHpUHmwKa0NApvn09AWbR5EK8nqtx2oUTVpj';
-const artistIds = [
-  '2GoeZ0qOTt6kjsWW4eA6LS',
-  '7hHDO4bJGlEaEHlY2lj1eZ',
+// Spotify API credentials
+const clientId = 'c186fe5232e44b5bb83c38cae9854309';
+const clientSecret = '02dc65d227964029b877d63a4f7e614a';
+
+// Variables to store token and API request
+let token;
+let request;
+
+// Array of Spotify artist IDs
+let ids = [
+  '4zCH9qm4R2DADamUHMCa6O',
+  '4PULA4EFzYTrxYvOVlwpiQ',
+  '5sSzCxHtgL82pYDvx2QyEU',
+  '5sSzCxHtgL82pYDvx2QyEU',
+  '5wJ1H6ud777odtZl5gG507',
+  '4svvMm4TQnkphZJfhLCzzv',
+  '2nWYQRy7Ikh7CyWnvZZouD',
   '4YRxDV8wJFPHPTeXepOstw',
-  '7uIbLdzzSEqnX0Pkrb56cR',
-  '7n2wHs1TKAczGzO7Dd2rGr',
-  '0C8ZW7ezQVs4URX5aX7Kqx',
   '0oOet2f43PA68X5RxKobEy',
-  '2o4R2rK7FetH40HTv0SUWl',
-  '00sCATpEvwH48ays7PlQFU',
-  '4zCH9qm4R2DADamUHMCa6O'
+  '7qjJw7ZM2ekDSahLXPjIlN'
 ];
 
-const dataContainer = document.getElementById("data-container");
+// URL for Spotify API request
+let url = `https://api.spotify.com/v1/artists?ids=${ids.join(',')}`;
 
-async function fetchArtistData(artistId) {
-  const artistUrl = `https://api.spotify.com/v1/artists/${artistId}`;
-  const headers = {
-    'Authorization': `Bearer ${token}`
-  };
-
+// Function to fetch an access token
+const getToken = async () => {
   try {
-    const response = await fetch(artistUrl, { headers });
-    const data = await response.json();
-    return data;
+    const result = await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
+      },
+      body: 'grant_type=client_credentials'
+    });
+    const data = await result.json();
+    token = data.access_token;
+    console.log(token);
+    urlAndRequest();
   } catch (error) {
-    console.error("Error fetching artist data:", error);
-    return null;
+    console.error("Error fetching token:", error);
   }
 }
 
-async function getData() {
+// Function to set up the URL and request headers
+const urlAndRequest = () => {
+  if (!token) {
+    document.write("Token not available yet.");
+    return;
+  }
+  request = new Request(url, {
+    headers: {
+      'Authorization': `Bearer ${token}`
+    },
+  });
+  fetchData();
+}
+
+// Function to fetch artist data
+const fetchData = async () => {
   try {
-    const artistDataPromises = artistIds.map(fetchArtistData);
-    const artistDataArray = await Promise.all(artistDataPromises);
+    const response = await fetch(request);
+    const data = await response.json();
+    console.log(data);
 
-    const artistList = document.createElement("ul");
+    // Get the data container element
+    const dataContainer = document.getElementById("assignment");
 
-    artistDataArray.forEach(artist => {
-      if (artist) {
-        const table=document.getElementById("artist-table")
-        const listItem = document.createElement("tr");
-        listItem.innerHTML = `
-       <td> <img src="${artist.images[0].url}" alt="${artist.name}'s Image"></td>
-         <td> <h3>Artist Name: ${artist.name}</h3></td>
-          <td><p>Follower: ${artist.followers.total}</p></td>
-          <td><p>Music Kind: ${artist.genres.join(', ')}</p></td>
-         <td> <p>Popularity: ${artist.popularity}</p></td>
-          
-        `;
-        table.appendChild(listItem);
+    // Loop through artists and create elements
+    data.artists.forEach(artist => {
+      if (artist.images.length > 0) {
+        const artistDiv = document.createElement("div");
+        artistDiv.classList.add("artist");
+
+        // Create and configure the artist image element
+        const artistImage = document.createElement("img");
+        artistImage.src = artist.images[2].url;
+        artistImage.alt = artist.name;
+
+        // Create and configure the artist name element
+        const artistname = document.createElement("h2");
+        artistname.textContent = `Artist Name: ${artist.name}`;
+
+        // Create and configure the artist genres element
+        const artistgenres = document.createElement("p");
+        artistgenres.textContent = `Artist Genres: ${artist.genres}`;
+
+        // Append elements to the artistDiv
+        artistDiv.appendChild(artistImage);
+        artistDiv.appendChild(artistname);
+        artistDiv.appendChild(artistgenres);
+
+        // Append artistDiv to the dataContainer
+        dataContainer.appendChild(artistDiv);
       }
     });
-
-    dataContainer.appendChild(artistList);
   } catch (error) {
-    console.error("Error fetching data:", error);
+    document.write("Error fetching data:", error);
   }
 }
 
-getData();
+// Call the getToken function to start the process
+getToken();
